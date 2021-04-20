bdc66ef4cb37444a2c97417fa6a34306d880e739
bymoye <964907582@qq.com>
2021-04-21 00:06:40 +0800

    修复一个番剧页的bug

--- inc/classes/Bilibili.php
+++ inc/classes/Bilibili.php
@@ -20,7 +20,7 @@ class Bilibili
             'headers' => array(
                 'Cookie' => $cookies,
                 'Host' => 'api.bilibili.com',
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97'
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
             )
         );
         $response = wp_remote_get($url, $args);
@@ -43,24 +43,26 @@ class Bilibili
                 $percent = 100;
             } else {
                 preg_match('/第(\d+)./m', $list['progress'], $matches_progress);
-                preg_match('/第(\d+)./m', $list["new_ep"]['index_show'], $matches_new);
+                preg_match('/第(\d+)./m', $list["new_ep"]['index_show']??null, $matches_new);
                 $progress = !empty($matches_progress[1]) ? (is_numeric($matches_progress[1]) ? $matches_progress[1] : 0) : 0;
                 $total = !empty($matches_progress[1]) ? (is_numeric($matches_new[1]) ? $matches_new[1] : $list['total_count']) : $list['total_count'];
                 $percent = $progress / $total * 100;
             }
-            $html .= '<div class="column">
-                <a class="bangumi-item" href="https://bangumi.bilibili.com/anime/' . $list['season_id'] . '/" target="_blank" rel="nofollow">
-                    <img class="bangumi-image" src="' . str_replace('http://', 'https://', $list['cover']) . '"/>
-                    <div class="bangumi-info">
-                        <h3 class="bangumi-title" title="' . $list['title'] . '">' . $list['title'] . '</h2>
-                        <div class="bangumi-summary"> ' . $list['evaluate'] . ' </div>
-                        <div class="bangumi-status">
-                            <div class="bangumi-status-bar" style="width: ' . $percent . '%"></div>
-                            <p>' . $list['new_ep']['index_show'] . '</p>         
+            if (isset($list['new_ep']['index_show'])){
+                $html .= '<div class="column">
+                    <a class="bangumi-item" href="https://bangumi.bilibili.com/anime/' . $list['season_id'] . '/" target="_blank" rel="nofollow">
+                        <img class="bangumi-image" src="' . str_replace('http://', 'https://', $list['cover']) . '"/>
+                        <div class="bangumi-info">
+                            <h3 class="bangumi-title" title="' . $list['title'] . '">' . $list['title'] . '</h2>
+                            <div class="bangumi-summary"> ' . $list['evaluate'] . ' </div>
+                            <div class="bangumi-status">
+                                <div class="bangumi-status-bar" style="width: ' . $percent . '%"></div>
+                                <p>' . $list['new_ep']['index_show'] . '</p>         
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>';
+                    </a>
+                </div>';
+            }
         }
         $html .= '</div><br><div id="bangumi-pagination">' . $next . '</div>';
         return $html;
